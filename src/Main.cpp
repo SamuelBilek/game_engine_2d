4a151fde2f1420d7853f7ac1114a5e5a7540d856
Samuel Bilek <sam@polygoniq.com>
2024-09-12 15:00:25 +0200

    Added Game class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <sol/sol.hpp>
+#include "Game.h"
 
 int main(int argc, char* argv[]) {
-	sol::state lua;
-	lua.open_libraries(sol::lib::base);
+	Game game;
 
-	glm::vec2 vector(1.0f, 2.0f);
-	SDL_Init(SDL_INIT_EVERYTHING);
-    std::cout << "Hello, world!" << std::endl;
+	game.Initialize();
+	game.Run();
+	game.Destroy();
+
     return 0;
 }
