4bebbd05040d65248969cbf974a4952d200f56d1
Samuel Bilek <sam@polygoniq.com>
2024-09-14 16:17:51 +0200

    SDL Window

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "SDL.h"
 #include <iostream>
 
 Game::Game() {
@@ -10,7 +11,29 @@ Game::~Game() {
 }
 
 void Game::Initialize() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "Error initializing SDL." << std::endl;
+		return;
+	}
 
+	SDL_Window* window = SDL_CreateWindow(
+		NULL, 
+		SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED, 
+		800, 
+		600, 
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		std::cerr << "Error creating SDL window." << std::endl;
+		return;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		std::cerr << "Error creating SDL renderer." << std::endl;
+		return;
+	}
 }
 
 void Game::Run() {
